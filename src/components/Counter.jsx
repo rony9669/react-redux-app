cdeb82a706dce2f11b54b70d56a75842bd9adebc
rony9669 <ronychowdhury.cse@gmail.com>
2023-01-02 03:01:16 +0600

    full website

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrementCounter,
+  incrementCounter,
+  resetCounter,
+} from "../redux/actions/counterAction";
 
 export const Counter = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  const handleInc = () => {
+    dispatch(incrementCounter());
+  };
+  const handleRes = () => {
+    dispatch(resetCounter());
+  };
+  const handleDec = () => {
+    dispatch(decrementCounter());
+  };
   return (
     <div>
       <h2> This a Counter App</h2>
-      <h3>Count: 0</h3>
+      <h3>Count: {count}</h3>
 
-      <Button variant="success">Success</Button>
-      <Button variant="light">Light</Button>
+      <Button onClick={handleInc} variant="success">
+        Increment
+      </Button>
+      <Button onClick={handleRes} variant="light">
+        rese
+      </Button>
+      <Button onClick={handleDec} variant="light">
+        decrement
+      </Button>
     </div>
   );
 };
